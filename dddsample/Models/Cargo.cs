031c4bd2c3c1fdc6498bb717b597ac9e6268ef14
Paul Rayner <paul@virtual-genius.com>
2012-11-30 11:14:14 -0700

    Add itinerary to Cargo

--- dddsample/Models/Cargo.cs
+++ dddsample/Models/Cargo.cs
@@ -6,6 +6,7 @@ namespace dddsample
         public RouteSpecification RouteSpecification { get; private set; }
         public TransportStatus TransportStatus { get; private set; }
         public Location LastKnownLocation { get; private set; }
+        public Itinerary Itinerary { get; private set; }
 
         public Cargo(RouteSpecification routeSpecification)
         {
@@ -33,6 +34,7 @@ namespace dddsample
 
         public void AssignToRoute(Itinerary itinerary)
         {
+            Itinerary = itinerary;
             RoutingStatus = RoutingStatus.Routed;
         }
     }
